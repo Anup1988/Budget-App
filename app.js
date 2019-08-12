b0bf24fde1a9a95f930dbab62e80c78a7ea6e0cb
Anup Prasai <prasai.anup@hotmail.com>
2019-08-11 21:39:28 -0400

    Reading Input Data: Assigning UI class name to a variable in UI Controller Module

--- app.js
+++ app.js
@@ -2,20 +2,43 @@
 var budgetController = (function() {})();
 
 //UI controller
-var UIController = (function() {})();
+var UIController = (function() {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either income or expenses
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+})();
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UIController.getDOMstrings();
+
   var ctrlAddItem = function() {
     // 1. Get the filled input data
+    var input = UICtrl.getInput();
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-    console.log("It works");
+    console.log(input);
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
